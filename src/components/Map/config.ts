b72fcb3bef98f874e862356ee71a282a31d6307d
Andreas Teneketzoglou <antekai00@gmail.com>
2024-08-27 23:56:52 +0200

    feat: update service worker to cache map tiles

--- src/components/Map/config.ts
+++ src/components/Map/config.ts
@@ -5,3 +5,9 @@ export const MAP_CENTER: LatLngTuple = [51.1657, 10.4515];
 // available colors https://github.com/pointhi/leaflet-color-markers
 export const SELECTED_MARKER_COLOR = "green";
 export const UNSELECTED_MARKER_COLOR = "red";
+
+export const ICON_URL =
+  "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-";
+
+export const ICON_SHADOW_URL =
+  "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png";
--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,35 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
       manifest: manifestJSON as Partial<ManifestOptions>,
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => {
+              const CACHE_URL_PATTERNS = [
+                "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-",
+                "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+                "openstreetmap.org",
+              ];
+
+              return CACHE_URL_PATTERNS.some((pattern) =>
+                url.href.includes(pattern)
+              );
+            },
+            handler: "CacheFirst",
+            options: {
+              cacheName: "map-tile-cache",
+              expiration: {
+                maxEntries: 1000,
+                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
       },
     }),
   ],
